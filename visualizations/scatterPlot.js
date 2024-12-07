4121ae2d79269515da9e1bf04c1662cc4deed72d
zqiu30 <125387554+zqiu30@users.noreply.github.com>
2024-12-06 22:49:31 -0700

    added comments

--- visualizations/scatterPlot.js
+++ visualizations/scatterPlot.js
@@ -1,27 +1,24 @@
 export function createScatterPlot(svg, data, options) {
-  const margin = { top: 40, right: 120, bottom: 80, left: 80 }; // Adjust left margin
+  const margin = { top: 40, right: 120, bottom: 80, left: 80 };
   const width = +svg.attr("width") - margin.left - margin.right;
   const height = +svg.attr("height") - margin.top - margin.bottom;
 
   const g = svg.append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // Create scales
+  // create scales
   const x = d3.scaleLinear()
     .range([0, width])
     .domain(d3.extent(data, d => +d[options["X Axis"]])).nice();
-
   const y = d3.scaleLinear()
     .range([height, 0])
     .domain(d3.extent(data, d => +d[options["Y Axis"]])).nice();
 
-
-  // Create color scale (using the selected "Color" field from options)
   const color = d3.scaleOrdinal(d3.schemeCategory10);
-  const colorDomain = Array.from(new Set(data.map(d => d[options["Color"]]))); // Unique categories
+  const colorDomain = Array.from(new Set(data.map(d => d[options["Color"]]))); // make sure that the  categories are unqiue
   color.domain(colorDomain);
 
-  // Add X axis
+  // add axises
   g.append("g")
     .attr("transform", `translate(0,${height})`)
     .call(d3.axisBottom(x))
@@ -30,21 +27,20 @@ export function createScatterPlot(svg, data, options) {
     .attr("dx", "-.8em")
     .attr("dy", ".15em")
     .attr("transform", "rotate(-45)");
-
-  // Add X axis label
-  svg.append("text")
-    .attr("transform", `translate(${width / 2 + margin.left},${height + margin.top + 40})`)
-    .style("text-anchor", "middle")
-    .style("font-size", "14px")
-    .text(options["X Axis"]);
-
-  // Add Y axis
+  // y
   g.append("g")
     .call(d3.axisLeft(y))
     .selectAll("text")
     .style("font-size", "12px");
 
-  // Add Y axis label
+
+  // add ticks for x
+  svg.append("text")
+    .attr("transform", `translate(${width / 2 + margin.left},${height + margin.top + 40})`)
+    .style("text-anchor", "middle")
+    .style("font-size", "14px")
+    .text(options["X Axis"]);
+  // y
   svg.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", margin.left / 1.3)
@@ -54,7 +50,7 @@ export function createScatterPlot(svg, data, options) {
     .style("font-size", "14px")
     .text(options["Y Axis"]);
 
-  // Add dots
+  // add dots
   g.selectAll(".dot")
     .data(data)
     .enter().append("circle")
@@ -62,10 +58,10 @@ export function createScatterPlot(svg, data, options) {
     .attr("r", 5)
     .attr("cx", d => x(+d[options["X Axis"]]))
     .attr("cy", d => y(+d[options["Y Axis"]]))
-    .style("fill", d => color(d[options["Color"]])) // Dynamically set color based on selected field
+    .style("fill", d => color(d[options["Color"]])) // make color based on what we chose
     .style("opacity", 0.7);
 
-  // Add legend
+  // legend on the left
   const legend = svg.append("g")
     .attr("font-family", "sans-serif")
     .attr("font-size", 10)
@@ -86,7 +82,7 @@ export function createScatterPlot(svg, data, options) {
     .attr("dy", "0.32em")
     .text(d => d);
 
-  // Add chart title
+  // title
   svg.append("text")
     .attr("x", (width / 2) + margin.left)
     .attr("y", margin.top / 2)
